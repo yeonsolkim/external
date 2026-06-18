26c64af3f92d227d42bddcf3ef3f3a178fe9bade
YeonsolKim <yeonsolkim@YeonsolKims-MacBook-Air.local>
2026-06-18 13:46:17 +0900

    2026-06-18 13:46

--- assets/js/mathjax-config.js
+++ assets/js/mathjax-config.js
@@ -1,126 +1,194 @@
-function normalizeInlineMathDelimiters() {
-  var roots = document.querySelectorAll('.post-body');
-  var skipTags = {
-    CODE: true,
-    PRE: true,
-    SCRIPT: true,
-    STYLE: true,
-    TEXTAREA: true,
-    NOSCRIPT: true
-  };
-
-  function isEscaped(text, index) {
-    var backslashes = 0;
-    index -= 1;
-
-    while (index >= 0 && text[index] === '\\') {
-      backslashes += 1;
+(function () {
+  function normalizeInlineMathDelimiters() {
+    function isEscaped(text, index) {
+      var backslashes = 0;
       index -= 1;
+
+      while (index >= 0 && text[index] === '\\') {
+        backslashes += 1;
+        index -= 1;
+      }
+
+      return backslashes % 2 === 1;
     }
 
-    return backslashes % 2 === 1;
-  }
+    function findInlineMathRanges(text) {
+      var ranges = [];
+      var start = -1;
 
-  function convertText(text) {
-    var result = '';
-    var start = -1;
+      for (var index = 0; index < text.length; index += 1) {
+        if (text[index] !== '$' || isEscaped(text, index)) {
+          continue;
+        }
 
-    for (var index = 0; index < text.length; index += 1) {
-      if (text[index] !== '$' || isEscaped(text, index)) {
-        continue;
+        if (text[index + 1] === '$' || text[index - 1] === '$') {
+          continue;
+        }
+
+        if (start === -1) {
+          start = index;
+        } else {
+          ranges.push({ start: start, end: index });
+          start = -1;
+        }
       }
 
-      if (text[index + 1] === '$' || text[index - 1] === '$') {
-        continue;
-      }
-
-      if (start === -1) {
-        start = index;
-      } else {
-        result += text.slice(0, start) + '\\(' + text.slice(start + 1, index) + '\\)';
-        text = text.slice(index + 1);
-        index = -1;
-        start = -1;
-      }
+      return ranges;
     }
 
-    return result + text;
+    function tokenizeHtml(html) {
+      var tokens = [];
+      var pattern = /<\/?em\b[^>]*>/gi;
+      var lastIndex = 0;
+      var match;
+
+      while ((match = pattern.exec(html))) {
+        if (match.index > lastIndex) {
+          tokens.push({
+            raw: html.slice(lastIndex, match.index),
+            synthetic: html.slice(lastIndex, match.index),
+            rawStart: lastIndex,
+            rawEnd: match.index
+          });
+        }
+
+        tokens.push({
+          raw: match[0],
+          synthetic: '_',
+          rawStart: match.index,
+          rawEnd: pattern.lastIndex
+        });
+
+        lastIndex = pattern.lastIndex;
+      }
+
+      if (lastIndex < html.length) {
+        tokens.push({
+          raw: html.slice(lastIndex),
+          synthetic: html.slice(lastIndex),
+          rawStart: lastIndex,
+          rawEnd: html.length
+        });
+      }
+
+      return tokens;
+    }
+
+    function buildSyntheticMap(tokens) {
+      var synthetic = '';
+      var map = [];
+
+      tokens.forEach(function (token) {
+        for (var index = 0; index < token.synthetic.length; index += 1) {
+          synthetic += token.synthetic[index];
+          map.push({
+            rawStart: token.rawStart + (token.raw === token.synthetic ? index : 0),
+            rawEnd: token.raw === token.synthetic ? token.rawStart + index + 1 : token.rawEnd
+          });
+        }
+      });
+
+      return {
+        synthetic: synthetic,
+        map: map
+      };
+    }
+
+    function normalizeHtml(html) {
+      var tokens = tokenizeHtml(html);
+      var mapped = buildSyntheticMap(tokens);
+      var ranges = findInlineMathRanges(mapped.synthetic);
+
+      if (ranges.length === 0) {
+        return html;
+      }
+
+      ranges.slice().reverse().forEach(function (range) {
+        var rawStart = mapped.map[range.start].rawStart;
+        var rawEnd = mapped.map[range.end].rawEnd;
+        var replacement = '\\(' + mapped.synthetic.slice(range.start + 1, range.end) + '\\)';
+        html = html.slice(0, rawStart) + replacement + html.slice(rawEnd);
+      });
+
+      return html;
+    }
+
+    var elements = document.querySelectorAll('.post-body p, .post-body li');
+
+    elements.forEach(function (element) {
+      if (element.closest('pre, code, script, style, textarea, noscript, mjx-container')) {
+        return;
+      }
+
+      if (element.innerHTML.indexOf('$') === -1) {
+        return;
+      }
+
+      element.innerHTML = normalizeHtml(element.innerHTML);
+    });
   }
 
-  roots.forEach(function (root) {
-    var walker = document.createTreeWalker(root, 4, {
-      acceptNode: function (node) {
-        var parent = node.parentElement;
+  function normalizeInlineMathWhenReady() {
+    if (document.readyState === 'loading') {
+      return new Promise(function (resolve) {
+        document.addEventListener('DOMContentLoaded', function () {
+          normalizeInlineMathDelimiters();
+          resolve();
+        }, { once: true });
+      });
+    }
 
-        while (parent && parent !== root) {
-          if (skipTags[parent.tagName] || parent.tagName === 'MJX-CONTAINER') {
-            return 2;
+    normalizeInlineMathDelimiters();
+    return Promise.resolve();
+  }
+
+  window.normalizeInlineMathDelimiters = normalizeInlineMathDelimiters;
+  normalizeInlineMathWhenReady();
+
+  window.MathJax = {
+    tex: {
+      inlineMath: [['\\(', '\\)']],
+      displayMath: [['$$', '$$'], ['\\[', '\\]']],
+      processEscapes: true
+    },
+    options: {
+      enableMenu: false
+    },
+    startup: {
+      pageReady() {
+        return normalizeInlineMathWhenReady().then(function () {
+          return MathJax.startup.defaultPageReady();
+        }).then(
+          () => {
+            document.documentElement.classList.remove('mathjax-loading');
+          },
+          (error) => {
+            document.documentElement.classList.remove('mathjax-loading');
+            throw error;
           }
-
-          parent = parent.parentElement;
-        }
-
-        return node.nodeValue.indexOf('$') === -1 ? 2 : 1;
+        );
       }
-    });
-    var nodes = [];
-    var node;
-
-    while ((node = walker.nextNode())) {
-      nodes.push(node);
+    },
+    output: {
+      font: 'mathjax-tex',
+      displayOverflow: 'linebreak',
+      linebreaks: {
+        inline: true,
+        width: '100%',
+        lineleading: 0.2
+      }
+    },
+    svg: {
+      fontCache: 'global',
+      scale: 0.89,
+      exFactor: 0.5,
+      displayAlign: 'center',
+      displayOverflow: 'linebreak',
+      linebreaks: {
+        inline: true,
+        width: '100%',
+        lineleading: 0.2
+      }
     }
-
-    nodes.forEach(function (textNode) {
-      textNode.nodeValue = convertText(textNode.nodeValue);
-    });
-  });
-}
-
-normalizeInlineMathDelimiters();
-
-window.MathJax = {
-  tex: {
-    inlineMath: [['\\(', '\\)']],
-    displayMath: [['$$', '$$'], ['\\[', '\\]']],
-    processEscapes: true
-  },
-  options: {
-    enableMenu: false
-  },
-  startup: {
-    pageReady() {
-      normalizeInlineMathDelimiters();
-
-      return MathJax.startup.defaultPageReady().then(
-        () => {
-          document.documentElement.classList.remove('mathjax-loading');
-        },
-        (error) => {
-          document.documentElement.classList.remove('mathjax-loading');
-          throw error;
-        }
-      );
-    }
-  },
-  output: {
-    font: 'mathjax-tex',
-    displayOverflow: 'linebreak',
-    linebreaks: {
-      inline: true,
-      width: '100%',
-      lineleading: 0.2
-    }
-  },
-  svg: {
-    fontCache: 'global',
-    scale: 0.89,
-    exFactor: 0.5,
-    displayAlign: 'center',
-    displayOverflow: 'linebreak',
-    linebreaks: {
-      inline: true,
-      width: '100%',
-      lineleading: 0.2
-    }
-  }
-};
+  };
+}());
